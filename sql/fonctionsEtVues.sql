ad14f1f2f28d13c52594029d5c6a8710e9cf7656
Cochaux Kyerann <kyerann.cochaux@etu.univ-lehavre.fr>
2026-05-05 09:55:30 +0200

    ajout des commentaires

---
+++ sql/fonctionsEtVues.sql
@@ -0,0 +1,38 @@
+/**
+  * Vues et fonctions
+  * Date : 5 mai 2026
+  * @author Jenny Richard, Antoine Bertin, Kyerann Cochaux
+  */
+
+
+
+-- 1. Créer une vue qui regroupe les médecins chercheurs (nom, mail, spécialité) et le nom
+--   de leur hôpital.
+
+
+
+
+
+-- 2. Utiliser cette vue pour afficher les médecins chercheurs de l'hôpital Jacques Monod.
+
+
+
+
+
+-- 3. Créer une vue contenant les premières consultations des patients (idPat,
+--   premiere_date).
+
+
+
+
+
+-- 4. Utiliser cette vue pour écrire une fonction age_patient qui admet comme paramètre
+--   l’identifiant du patient et qui retourne l’âge du patient à sa première consultation.
+
+
+
+
+
+-- 5. Écrire une requête qui donne la liste des consultations (nom du médecin, nom du
+--   patient) des patients de plus de 40 ans. Utiliser la fonction age_patient.
+
